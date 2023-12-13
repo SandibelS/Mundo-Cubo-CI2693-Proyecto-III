a197b2cdbd3472b2666a66396cbbea48c99c815f
SandibelS <17-10614@usb.ve>
2023-12-13 12:59:01 -0500

    Added aux files

---
+++ Pair.java
@@ -0,0 +1,9 @@
+public class Pair<T, U>{
+    T val1;
+    U val2;
+
+    Pair(T value1, U value2){
+        this.val1 = value1;
+        this.val2 = value2;
+    }
+}
---
+++ Triple.java
@@ -0,0 +1,12 @@
+public class Triple <T>{
+    public T val1;
+    public T val2;
+    public int heigh;
+
+    public Triple(T val1, T val2, int cost){
+        this.val1 = val1;
+        this.val2 = val2;
+        this.heigh = cost;
+    }
+
+}
